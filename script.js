ecd44346675419034aef41d3bc1a596e4c218432
sonaljain01 <108785075+sonaljain01@users.noreply.github.com>
2024-08-07 12:21:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function favouriteMivieGenre(genre) {
+    console.log("Favorite movie genre is: " + genre);
+}
+
+function favouriteFruit(fruit) {
+    console.log("Favorite fruit is: " + fruit);
+}
+
+function favouriteMode(mode) {
+    console.log("Favorite mode is: " + mode);
+}
+
+function favouriteEdgeStyle(style) {
+    console.log("Favorite edge style is: " + style);
+}
+
+favouriteMivieGenre("cowboy")
+favouriteFruit("banana")
+favouriteMode("light")
+favouriteEdgeStyle("smooth")
